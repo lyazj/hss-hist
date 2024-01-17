b6492f15782fe21874c315d29df94af6d0f3c44c
lyazj <seeson@pku.edu.cn>
2024-01-17 17:44:21 +0100

    Not check directory mtime.
    Note that all files are located in the same directory.

--- src/merge.py
+++ src/merge.py
@@ -23,9 +23,7 @@ def get_mtime(path):
 
 def update_merged_root_file(filein, fileout):
     print('Checking:', fileout)
-    dirnames = sorted(set(os.path.dirname(f) for f in filein))
-    if len(dirnames) != 1: raise RuntimeError('expect exact 1 directory, got %s' % dirnames)
-    mtime_in = max(get_mtime(dirnames[0]), max(get_mtime(f) for f in filein))
+    mtime_in = max(get_mtime(f) for f in filein)
     mtime_out = get_mtime(fileout)
     if mtime_in < mtime_out: return
     merge_root_files(filein, fileout)
