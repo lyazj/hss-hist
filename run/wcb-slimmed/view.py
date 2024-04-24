43679efb0c82ded114f7643b9aa316036ede06e2
lyazj <seeson@pku.edu.cn>
2024-04-24 13:07:37 +0800

    Add view.py.

--- run/wcb-slimmed/view.py
+++ run/wcb-slimmed/view.py
@@ -8,14 +8,12 @@ import numpy as np
 import awkward as ak
 import mplhep as hep
 import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
 
 # Transcript printed content to a same-name log file.
 logfile = open(re.sub(r'\.py$', '.log', __file__), 'w')
 def print(*args, **kwargs): builtins.print(*args, **kwargs); builtins.print(*args, **{**kwargs, 'file': logfile})
 
 plt.style.use(hep.style.CMS)
-gs = gridspec.GridSpec(2, 1, height_ratios=[4, 1])
 
 NEVENT_MAX = None
 #NEVENT_MAX = 1000000
@@ -108,7 +106,6 @@ categories = list(events.keys())
 
 def figure(*args, **kwargs):
     fig = plt.figure(*args, **kwargs)
-    fig.add_subplot(gs[0])
     try:
         hep.cms.label(data=False, paper=False, supplementary=False, year=2018, lumi=59.7)
     except Exception:
@@ -132,78 +129,10 @@ def savefig(path, *args, **kwargs):
     print('Saving to %s...' % path)
     plt.savefig(path, *args, **kwargs)
 
-def get_signif(s, b):
-    return np.sqrt(2 * ((s + b) * np.log(1 + s / (b + (s == 0))) - s))
-
-def signif(hists, cates):
-    wcb_hists  = [hist    for (hist, cate) in zip(hists, cates) if cate == 'Wcb']
-    hists      = [hist    for (hist, cate) in zip(hists, cates) if cate != 'Wcb']
-    bins = wcb_hists[0][1]
-    for hist in wcb_hists + hists:
-        if np.any(hist[1] != bins): raise NotImplementedError('rebinning not implemented')
-        if hist[0].shape[0] + 1 != hist[1].shape[0]: raise ValueError('incompatible bin count size')
-    s_cumsum = np.sum([np.cumsum(np.concatenate([[0], count])) for (count, _) in wcb_hists], axis=0)
-    b_cumsum = np.sum([np.cumsum(np.concatenate([[0], count])) for (count, _) in hists    ], axis=0)
-    signif = np.zeros((len(bins), len(bins)))
-    for imin in range(len(bins) - 1):
-        for imax in range(imin + 1, len(bins)):
-            s = s_cumsum[imax] - s_cumsum[imin]
-            b = b_cumsum[imax] - b_cumsum[imin]
-            signif[imin, imax] = get_signif(s, b)
-    signif_l = signif.max(axis=1)
-    signif_u = signif.max(axis=0)
-    l = signif_l.argmax()
-    u = signif_u.argmax()
-    signif_max = signif_l[l]
-    plt.plot(bins, signif_l, label='lower')
-    plt.plot(bins, signif_u, label='upper')
-    plt.plot([bins[l]] * 2, [0, signif_max * 1.2], 'k--')
-    plt.plot([bins[u]] * 2, [0, signif_max * 1.2], 'k--')
-    plt.plot([], [], 'k--', label='optimal (%.3f)' % signif_max)
-    plt.legend()
-    return signif_max
-
-print('B veto:')
-cut_events = { }
-for category in categories:
-    cut_events[category] = events[category][events[category]['nb'] == 0]
-    print('  - %s:\t%d' % (category, len(cut_events[category])))
-events = cut_events
-
-thresholds = [
-    [0.990, 0.995, 0.998, 0.999],
-    [0.950, 0.980, 0.990, 0.995],
-    [0.800, 0.900, 0.950, 0.980],
-]
-
-for iSR in range(len(thresholds)):
-
-    s_best = -1.0
-    for threshold in thresholds[iSR]:
-
-        print('Cut HbcVSQCS >= %.3f:' % threshold)
-        cut_events = { }
-        for category in categories:
-            cut_events[category] = events[category][events[category]['a_HbcVSQCS'] >= threshold]
-            print('  - %s:\t%d' % (category, len(cut_events[category])))
-
-        fig = figure(figsize=(12, 11.25), dpi=150)
-        sdmass_bins = np.linspace(20, 220, 51)
-        sdmass_hists = [np.histogram(cut_events[category]['a_sdmass'], sdmass_bins, weights=cut_events[category]['weight']) for category in categories]
-        histplot(sdmass_hists, categories)
-        plt.ylabel('Events'); plt.yscale('log'); plt.legend(); plt.grid()
-        plt.gca().set_xticklabels([]); fig.add_subplot(gs[1])
-        s = signif(sdmass_hists, categories)
-        plt.xlabel('Soft Dropped Mass [GeV]'); plt.ylabel('Significance'); plt.grid()
-        plt.tight_layout(); savefig('sr%d-%.3f.pdf' % (iSR + 1, threshold))
-        plt.close()
-
-        if s > s_best: s_best = s; threshold_best = threshold
-
-    threshold = threshold_best
-    print('Cut HbcVSQCS < %.3f:' % threshold)
-    cut_events = { }
-    for category in categories:
-        cut_events[category] = events[category][events[category]['a_HbcVSQCS'] < threshold]
-        print('  - %s:\t%d' % (category, len(cut_events[category])))
-    events = cut_events
+figure(figsize=(12, 9), dpi=150)
+nb_bins = np.linspace(0, 8.0, 9)
+nb_hists = [np.histogram(events[category]['nb'], nb_bins, weights=events[category]['weight']) for category in labels.keys()]
+histplot(nb_hists, labels.keys())
+plt.xlabel('$N_b$'); plt.ylabel('Events'); plt.yscale('log')
+plt.legend(); plt.grid(); plt.tight_layout(); savefig('view-nb.pdf')
+plt.close()
